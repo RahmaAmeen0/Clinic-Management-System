b5b65b4f08af02c1cc609dea08d510d0182b361d
Mostafa Mohamed <sase1351@gmail.com>
2026-05-15 01:57:00 +0300

    Added AppointmentController and Layout fixes

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,28 @@
+﻿using ClinicManagementSystem.Models;
+using ClinicManagementSystem.Repositories.IRepositories;
+using ClinicManagementSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ClinicManagementSystem.Controllers
+{
+    public class AppointmentController : Controller 
+    {
+        private readonly IPatientRepository _patientRepo;
+        private readonly IDepartmentRepository _departmentRepo;
+        private readonly IAppointmentRepository _appointmentRepo;
+        private readonly IDoctorRepository _doctorRepo;
+        private readonly IDoctorScheduleRepository _scheduleRepo;
+        public AppointmentController(IPatientRepository patientRepo,
+            IDepartmentRepository departmentRepo, IAppointmentRepository appointmentRepo,
+            IDoctorRepository doctorRepo, IDoctorScheduleRepository scheduleRepo)
+        {
+            _patientRepo = patientRepo;
+            _departmentRepo = departmentRepo;
+            _appointmentRepo = appointmentRepo;
+            _doctorRepo = doctorRepo;
+            _scheduleRepo = scheduleRepo;
+        }
+        
+    }
+}
