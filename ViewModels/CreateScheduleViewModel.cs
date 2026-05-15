a77882b0a4c183d322a4fe6091cf11f35313cc20
Samira Rashad <samirarashad24@gmail.com>
2026-05-15 20:47:36 +0300

    Finish doctor schedules UI adjustments and flow linking with appointment module

---
+++ ViewModels/CreateScheduleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManagementSystem.ViewModels
+{
+    public class CreateScheduleViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public int DoctorId { get; set; }
+
+        [Required(ErrorMessage = "Please select a work day.")]
+        public string WorkDay { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select a start time.")]
+        public TimeSpan StartTime { get; set; }
+
+        [Required(ErrorMessage = "Please select an end time.")]
+        public TimeSpan EndTime { get; set; }
+    }
+}
